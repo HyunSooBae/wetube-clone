97d349d353fa929a42298dd9d0026e7fe28c2b8c
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-01-04 02:14:04 +0900

    feat: app.get test

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000
 
 const app = express()
 
+const handleHome = (res, req) => {
+  return res.send('This is home...')
+}
+
+const handleLogin = (res, req) => {
+  return res.send('Login here...')
+}
+
+app.get('/', handleHome)
+app.get('/login', handleLogin)
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`)
 
